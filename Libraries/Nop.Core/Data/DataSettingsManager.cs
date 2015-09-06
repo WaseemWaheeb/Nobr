ae41e15521cb5252bea07c427637b68c7ce28532
Tay Wei Ru <weiru85@hotmail.com>
2015-09-07 00:21:18 +0800

    undo reading connection string from web.config

--- Libraries/Nop.Core/Data/DataSettingsManager.cs
+++ Libraries/Nop.Core/Data/DataSettingsManager.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Web;
-using System.Web.Configuration;
 using System.Web.Hosting;
 
 namespace Nop.Core.Data
@@ -12,25 +10,112 @@ namespace Nop.Core.Data
     /// </summary>
     public partial class DataSettingsManager
     {
+        protected const char separator = ':';
+        protected const string filename = "Settings.txt";
+
+        /// <summary>
+        /// Maps a virtual path to a physical disk path.
+        /// </summary>
+        /// <param name="path">The path to map. E.g. "~/bin"</param>
+        /// <returns>The physical path. E.g. "c:\inetpub\wwwroot\bin"</returns>
+        protected virtual string MapPath(string path)
+        {
+            if (HostingEnvironment.IsHosted)
+            {
+                //hosted
+                return HostingEnvironment.MapPath(path);
+            }
+
+            //not hosted. For example, run in unit tests
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            path = path.Replace("~/", "").TrimStart('/').Replace('/', '\\');
+            return Path.Combine(baseDirectory, path);
+        }
+
+        /// <summary>
+        /// Parse settings
+        /// </summary>
+        /// <param name="text">Text of settings file</param>
+        /// <returns>Parsed data settings</returns>
+        protected virtual DataSettings ParseSettings(string text)
+        {
+            var shellSettings = new DataSettings();
+            if (String.IsNullOrEmpty(text))
+                return shellSettings;
+
+            //Old way of file reading. This leads to unexpected behavior when a user's FTP program transfers these files as ASCII (\r\n becomes \n).
+            //var settings = text.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            var settings = new List<string>();
+            using (var reader = new StringReader(text))
+            {
+                string str;
+                while ((str = reader.ReadLine()) != null)
+                    settings.Add(str);
+            }
+
+            foreach (var setting in settings)
+            {
+                var separatorIndex = setting.IndexOf(separator);
+                if (separatorIndex == -1)
+                {
+                    continue;
+                }
+                string key = setting.Substring(0, separatorIndex).Trim();
+                string value = setting.Substring(separatorIndex + 1).Trim();
+
+                switch (key)
+                {
+                    case "DataProvider":
+                        shellSettings.DataProvider = value;
+                        break;
+                    case "DataConnectionString":
+                        shellSettings.DataConnectionString = value;
+                        break;
+                    default:
+                        shellSettings.RawDataSettings.Add(key, value);
+                        break;
+                }
+            }
+
+            return shellSettings;
+        }
+
+        /// <summary>
+        /// Convert data settings to string representation
+        /// </summary>
+        /// <param name="settings">Settings</param>
+        /// <returns>Text</returns>
+        protected virtual string ComposeSettings(DataSettings settings)
+        {
+            if (settings == null)
+                return "";
+
+            return string.Format("DataProvider: {0}{2}DataConnectionString: {1}{2}",
+                                 settings.DataProvider,
+                                 settings.DataConnectionString,
+                                 Environment.NewLine
+                );
+        }
+
         /// <summary>
         /// Load settings
         /// </summary>
+        /// <param name="filePath">File path; pass null to use default settings file path</param>
         /// <returns></returns>
-        public virtual DataSettings LoadSettings()
+        public virtual DataSettings LoadSettings(string filePath = null)
         {
-            try
+            if (String.IsNullOrEmpty(filePath))
             {
-                System.Configuration.Configuration webConfig = WebConfigurationManager.OpenWebConfiguration(HttpRuntime.AppDomainAppVirtualPath);
-                return new DataSettings
-                {
-                    DataConnectionString = webConfig.ConnectionStrings.ConnectionStrings["DefaultConnection"].ConnectionString,
-                    DataProvider = webConfig.ConnectionStrings.ConnectionStrings["DefaultConnection"].ProviderName
-                };
+                //use webHelper.MapPath instead of HostingEnvironment.MapPath which is not available in unit tests
+                filePath = Path.Combine(MapPath("~/App_Data/"), filename);
             }
-            catch (NullReferenceException)
+            if (File.Exists(filePath))
             {
-                return new DataSettings();
+                string text = File.ReadAllText(filePath);
+                return ParseSettings(text);
             }
+
+            return new DataSettings();
         }
 
         /// <summary>
@@ -39,14 +124,21 @@ namespace Nop.Core.Data
         /// <param name="settings"></param>
         public virtual void SaveSettings(DataSettings settings)
         {
-            if (null == settings) throw new ArgumentNullException("settings");
+            if (settings == null)
+                throw new ArgumentNullException("settings");
 
-            System.Configuration.Configuration webConfig = WebConfigurationManager.OpenWebConfiguration(HttpRuntime.AppDomainAppVirtualPath);
+            //use webHelper.MapPath instead of HostingEnvironment.MapPath which is not available in unit tests
+            string filePath = Path.Combine(MapPath("~/App_Data/"), filename);
+            if (!File.Exists(filePath))
+            {
+                using (File.Create(filePath))
+                {
+                    //we use 'using' to close the file after it's created
+                }
+            }
 
-            webConfig.ConnectionStrings.ConnectionStrings["DefaultConnection"].ConnectionString = settings.DataConnectionString;
-            webConfig.ConnectionStrings.ConnectionStrings["DefaultConnection"].ProviderName = settings.DataProvider;
-
-            webConfig.Save();
+            var text = ComposeSettings(settings);
+            File.WriteAllText(filePath, text);
         }
     }
 }
