158667eb6880ef56d5252c7d3bcd530bfcaa85d8
Tay Wei Ru <weiru85@hotmail.com>
2015-11-07 19:28:06 +0800

    fixed broken plugin

--- Plugins/Nop.Plugin.Widgets.SocialLinks/Controllers/WidgetsSocialLinksController.cs
+++ Plugins/Nop.Plugin.Widgets.SocialLinks/Controllers/WidgetsSocialLinksController.cs
@@ -13,32 +13,19 @@ namespace Nop.Plugin.Widgets.SocialLinks.Controllers
 {
     public class WidgetsSocialLinksController : BasePluginController
     {
-        private readonly string _viewFolderPath = "~/Plugins/Widgets.SocialLinks/Views/WidgetsSocialLinks";
+        private StoreInformationSettings _storeInformationSettings;
 
-        private readonly ISettingService _settingService;
-        private readonly StoreInformationSettings _storeInformationSettings;
-        private readonly IStoreService _storeService;
-        private readonly IWorkContext _workContext;
-
-        public WidgetsSocialLinksController(StoreInformationSettings storeInformationSettings, IStoreService storeService, IWorkContext workContext, ISettingService settingService)
+        public WidgetsSocialLinksController(StoreInformationSettings storeInformationSettings)
         {
             _storeInformationSettings = storeInformationSettings;
-            _storeService = storeService;
-            _workContext = workContext;
-            _settingService = settingService;
         }
 
         [AdminAuthorize]
         [ChildActionOnly]
         public ActionResult Configure()
         {
-            var model = new ConfigurationModel()
-            {
-                FacebookUrl = _storeInformationSettings.FacebookLink,
-                TwitterUrl = _storeInformationSettings.TwitterLink
-            };
-
-            return View(string.Format("{0}/{1}", _viewFolderPath, "Configure.cshtml"), model);
+            var model = new ConfigurationModel();
+            return View("~/Plugins/Widgets.SocialLinks/Views/WidgetsSocialLinks/Configure.cshtml", model);
         }
 
         [HttpPost]
@@ -46,24 +33,6 @@ namespace Nop.Plugin.Widgets.SocialLinks.Controllers
         [ChildActionOnly]
         public ActionResult Configure(ConfigurationModel model)
         {
-            var storeScope = this.GetActiveStoreScopeConfiguration(_storeService, _workContext);
-            var storeInformationSettings = _settingService.LoadSetting<StoreInformationSettings>(storeScope);
-
-            storeInformationSettings.FacebookLink = model.FacebookUrl;
-            storeInformationSettings.TwitterLink = model.TwitterUrl;
-
-            if (!string.IsNullOrWhiteSpace(storeInformationSettings.FacebookLink))
-                _settingService.SaveSetting(storeInformationSettings, x => x.FacebookLink, storeScope, false);
-            else
-                _settingService.DeleteSetting(storeInformationSettings, x => x.FacebookLink, storeScope);
-
-            if (!string.IsNullOrWhiteSpace(storeInformationSettings.TwitterLink))
-                _settingService.SaveSetting(storeInformationSettings, x => x.TwitterLink, storeScope, false);
-            else
-                _settingService.DeleteSetting(storeInformationSettings, x => x.TwitterLink, storeScope);
-
-            _settingService.ClearCache();
-
             return Configure();
         }
 
@@ -76,7 +45,7 @@ namespace Nop.Plugin.Widgets.SocialLinks.Controllers
                 TwitterUrl = _storeInformationSettings.TwitterLink
             };
 
-            return View(string.Format("{0}/{1}", _viewFolderPath, "PublicInfo.cshtml"), model);
+            return View("~/Plugins/Widgets.SocialLinks/Views/WidgetsSocialLinks/PublicInfo.cshtml", model);
         }
     }
 }
