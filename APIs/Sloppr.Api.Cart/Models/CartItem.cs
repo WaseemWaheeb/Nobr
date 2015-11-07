33bf4a553bd09e291834599d2d720308f28764f8
Tay Wei Ru <weiru85@hotmail.com>
2015-11-07 17:54:39 +0800

    added Web API project

---
+++ APIs/Sloppr.Api.Cart/Models/CartItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sloppr.Api.Cart.Models
+{
+    public class CartItem
+    {
+        public Guid Id { get; set; }
+        public string StoreId { get; set; }
+        public string CartTypeId { get; set; }
+        public string CustomerId { get; set; }
+        public string ProductId { get; set; }
+    }
+}
